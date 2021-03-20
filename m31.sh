2a765e7f6c4020e52c4e69102803bb57477724be
Yilliee <m.taha6f@gmail.com>
2021-03-20 03:55:55 +0000

    Add build script for M31

---
+++ m31.sh
@@ -0,0 +1,6 @@
+export ARCH=arm64
+make clean && make mrproper
+# make bluefly_a51_defconfig O=a51
+make bluefly_m31_defconfig O=m31
+# make -j$(nproc --all) CROSS_COMPILE=$HOME/toolchain/bin/aarch64-linux-android- CC=$HOME/clang/bin/clang CLANG_TRIPLE=aarch64-linux-gnu- O=a51
+make -j$(nproc --all) CROSS_COMPILE=$HOME/toolchain/bin/aarch64-linux-android- CC=$HOME/clang/bin/clang CLANG_TRIPLE=aarch64-linux-gnu- O=m31
